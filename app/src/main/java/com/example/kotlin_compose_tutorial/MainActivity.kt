1b98c427006efcac19c10fcde01ff004e4c8ad3c
ParkJong-Hun <thesn515@gmail.com>
2022-02-01 10:00:07 +0900

    2022/02/01 10:00 Box

--- app/src/main/java/com/example/kotlin_compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_compose_tutorial/MainActivity.kt
@@ -3,49 +3,35 @@ package com.example.kotlin_compose_tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.kotlin_compose_tutorial.ui.theme.Kotlin_ComposeTutorialTheme
-//chapter2
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+//chapter3
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContent 안에서는 Composable만 들어갈 수 있음
         setContent {
-            Kotlin_ComposeTutorialTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting(name = "Android")
+            //Box는 자식뷰들이 다 겹쳐져서 보임(FrameLayout과 비슷함)
+            Box(
+                modifier = Modifier
+                    .background(color = Color.Green)
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Hello World")
+                Box(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.BottomEnd
+                ) {
+                    Text(text = "Hello!!!")
                 }
             }
         }
-    }
-}
-
-//Composable annotation이 붙어있는 함수는 보통 대문자로 시작
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-//미리보기
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Kotlin_ComposeTutorialTheme {
-        Greeting("Park JongHun")
-    }
-}
-
-//미리보기2
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview2() {
-    Kotlin_ComposeTutorialTheme {
-        Greeting("Android")
     }
 }
