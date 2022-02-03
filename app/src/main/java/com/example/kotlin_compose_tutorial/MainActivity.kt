ce3a667e0124a9f869db6e6495cc807d3e074dc8
ParkJong-Hun <thesn515@gmail.com>
2022-02-03 10:04:47 +0900

    2022/02/03 10:04 Image, Card, remember

--- app/src/main/java/com/example/kotlin_compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_compose_tutorial/MainActivity.kt
@@ -3,46 +3,100 @@ package com.example.kotlin_compose_tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+//by 사용 가능
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-//chapter4
+
+//chapter5
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            /*
-            //스크롤의 상태 저장
-            val scrollState = rememberScrollState()
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    //수직 스크롤 가능
-                    .verticalScroll(scrollState)
-            ) {
-                //for문으로 여러 개 작성이 가능하다.
-                for (i in 1..50) {
-                    Text(text = "Text $i")
-                }
+            //전역에서 관리
+            var isFavorite by rememberSaveable {
+                mutableStateOf(false)
             }
-            */
-            //일반 Column과 다르게 화면에 표시되어야 할 때 UI을 보이는 연산을 시작.
-            LazyColumn(
-                modifier = Modifier.fillMaxWidth(), //수직 스크롤 가능
-                contentPadding = PaddingValues(16.dp), //상하좌우에 패딩 적용
-                verticalArrangement = Arrangement.spacedBy(8.dp) //뷰 간의 수직 간격
+            ImageCard(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)/*fraction: 전체에서의 비율*/
+                    .padding(16.dp),
+                isFavorite = isFavorite,
+            ) { favorite ->
+                isFavorite = favorite
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    modifier: Modifier = Modifier,
+    isFavorite: Boolean,
+    onTabFavorite: (Boolean) -> Unit/*전역에서 값 변경을 위한 Callback*/,
+) {
+    /*Composable에서 상태를 기억하는 변수
+    val isFavorite = remember {
+        mutableStateOf(false)
+    }
+    */
+    /*.value를 생략하기 위해 by 사용
+    var isFavorite by remember {
+        mutableStateOf(false)
+    }
+    */
+    /*항상 메모리에 저장되게 하려면 rememberSaveable
+    var isFavorite by rememberSaveable {
+        mutableStateOf(false)
+    }
+    */
+
+    //카드 모양 뷰
+    Card(
+        modifier = modifier/*외부에서 지정*/,
+        shape = RoundedCornerShape(8.dp)/*모양*/,
+        elevation = 5.dp/*부모 뷰로부터의 높이*/,
+    ) {
+        Box(
+            modifier = Modifier.height(200.dp)
+        ) {
+            //이미지 뷰
+            Image(
+                painter = painterResource(id = R.drawable.poster)/*이미지 자료*/,
+                contentDescription = "poster",
+                contentScale = ContentScale.Crop,//이미지의 크기 비율
+                modifier = Modifier.fillMaxSize()
+            )
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopEnd
             ) {
-                //LazyColumn에서는 for문 사용 불가능.
-                //for문 대신 사용. 여러 뷰를 반복해서 작성
-                items(50) { index ->
-                    Text(text = " Text $index")
+                //아이콘 버튼 뷰
+                IconButton(onClick = {
+                    onTabFavorite(!isFavorite)
+                }) {
+                    //아이콘 이미지 뷰
+                    Icon(imageVector = if(isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder/*아이콘 이미지*/,
+                        contentDescription = "favorite",
+                        tint = Color.White/*색상*/
+                    )
                 }
             }
         }
