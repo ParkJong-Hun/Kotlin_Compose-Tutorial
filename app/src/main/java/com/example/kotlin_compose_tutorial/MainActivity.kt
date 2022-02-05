6a7e83f00b87968204d600b05d2a90c3a6fb0d53
ParkJong-Hun <thesn515@gmail.com>
2022-02-05 10:52:58 +0900

    2022/02/05 10:52 Navigation

--- app/src/main/java/com/example/kotlin_compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_compose_tutorial/MainActivity.kt
@@ -1,60 +1,120 @@
 package com.example.kotlin_compose_tutorial
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import kotlinx.coroutines.launch
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
-//chapter6
-@ExperimentalComposeUiApi
+//chapter7
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val (text, setValue) = remember {
-                mutableStateOf("")
-            }//구조분해라는 기법을 활용할 수 있다.
-            val scaffoldState = rememberScaffoldState()//scaffold의 상태를 기억
-            val scope = rememberCoroutineScope()//적당한 CoroutineScope를 자동으로 만들어줌
-            val keyboardController = LocalSoftwareKeyboardController.current//키보드 조절
+            val navController = rememberNavController()//네비게이션 컨트롤러
 
-            //액자 뷰(여러 기능을 활용 가능 ex/ 스낵바)
-            Scaffold {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    //텍스트 입력 뷰
-                    TextField(
-                        value = text,
-                        onValueChange = setValue//값이 입력되었을 때의 이벤트 처리
+            //네비게이션 루트 관리
+            NavHost(
+                navController = navController,//사용한 컨트롤러
+                startDestination = "first"//처음 위치
+            ) {
+                //루트1
+                composable("first") {
+                    FirstScreen(navController = navController)
+                }
+                //루트2
+                composable("second") {
+                    SecondScreen(navController = navController)
+                }
+                //루트3
+                composable("third/{value}")/**Value를 가져옴**/ { backStackEntry ->
+                    ThirdScreen(
+                        navController = navController,
+                        //backStackEntry로 가져온 값을 확인하여 파라미터로 전달
+                        value= backStackEntry.arguments?.getString("value") ?: ""
                     )
-                    //버튼 뷰
-                    Button(onClick = {
-                        scope.launch {
-                            //스낵바 표시
-                            scaffoldState.snackbarHostState.showSnackbar("Hello $text")
-                        }
-                        //키보드 가리기
-                        keyboardController?.hide()
-                    }/*클릭 이벤트 처리*/) {
-                        Text("클릭")
-                    }
                 }
             }
+
+        }
+    }
+}
+
+@Composable
+fun FirstScreen(navController: NavController) {
+    val (value, setValue) = remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "첫 화면")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            navController.navigate("second")//네비게이션 컨트롤러로 해당 루트로 이동
+        }) {
+            Text("두 번째!")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(value = value, onValueChange = setValue)
+        Button(onClick = {
+            if(value.isNotEmpty()) {
+                navController.navigate("third/$value")//루트로 이동할 때 값을 전달
+            }
+        }) {
+            Text("세 번째!")
+        }
+    }
+}
+
+@Composable
+fun SecondScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "두 번째 화면")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            navController.navigateUp()
+            //navController.popBackStack()
+        }) {
+            Text("뒤로 가기!")
+        }
+    }
+}
+
+@Composable
+fun ThirdScreen(navController: NavController, value: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "세 번째 화면")
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = value)
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            navController.navigateUp()
+        }) {
+            Text("뒤로 가기!")
         }
     }
 }
