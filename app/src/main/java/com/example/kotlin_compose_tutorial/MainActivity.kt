0545d7777e0266b9c27aaec218eaae1e755d567b
ParkJong-Hun <thesn515@gmail.com>
2022-02-06 10:59:34 +0900

    2022/02/06 10:59 viewModel

--- app/src/main/java/com/example/kotlin_compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_compose_tutorial/MainActivity.kt
@@ -3,48 +3,49 @@ package com.example.kotlin_compose_tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
-//chapter7
+//chapter8
 class MainActivity : ComponentActivity() {
+    //private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()//네비게이션 컨트롤러
+            val viewModel = viewModel<MainViewModel>()//implementation "androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0" 할 경우 setContent 안에서 사용 가능
+            //val data = remember{mutableStateOf("Hello")}//remember를 사용하지 않으면, UI가 갱신될 때마다 setContent가 실행되어 data가 초기화됨
 
-            //네비게이션 루트 관리
-            NavHost(
-                navController = navController,//사용한 컨트롤러
-                startDestination = "first"//처음 위치
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                //루트1
-                composable("first") {
-                    FirstScreen(navController = navController)
-                }
-                //루트2
-                composable("second") {
-                    SecondScreen(navController = navController)
-                }
-                //루트3
-                composable("third/{value}")/**Value를 가져옴**/ { backStackEntry ->
-                    ThirdScreen(
-                        navController = navController,
-                        //backStackEntry로 가져온 값을 확인하여 파라미터로 전달
-                        value= backStackEntry.arguments?.getString("value") ?: ""
-                    )
+                Text(
+                    viewModel.data.value,
+                    fontSize = 30.sp
+                )
+                Button(onClick = {
+                    viewModel.changeValue()
+                }) {
+                    Text("변경")
                 }
             }
 
@@ -52,69 +53,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun FirstScreen(navController: NavController) {
-    val (value, setValue) = remember {
-        mutableStateOf("")
-    }
+class MainViewModel: ViewModel() {
+    //viewModel에서는 Remember가 없어도 유지가 된다.
+    private val _data = mutableStateOf("Hello")
+    val data: State<String> = _data
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "첫 화면")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            navController.navigate("second")//네비게이션 컨트롤러로 해당 루트로 이동
-        }) {
-            Text("두 번째!")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        TextField(value = value, onValueChange = setValue)
-        Button(onClick = {
-            if(value.isNotEmpty()) {
-                navController.navigate("third/$value")//루트로 이동할 때 값을 전달
-            }
-        }) {
-            Text("세 번째!")
-        }
-    }
-}
-
-@Composable
-fun SecondScreen(navController: NavController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "두 번째 화면")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            navController.navigateUp()
-            //navController.popBackStack()
-        }) {
-            Text("뒤로 가기!")
-        }
-    }
-}
-
-@Composable
-fun ThirdScreen(navController: NavController, value: String) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "세 번째 화면")
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = value)
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            navController.navigateUp()
-        }) {
-            Text("뒤로 가기!")
-        }
+    fun changeValue() {
+        _data.value = "World"
     }
 }
